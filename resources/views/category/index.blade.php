689f0f6acc8d2e2209ba6d0ef3449f69e0dc5e02
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-12-06 22:46:25 +0600

    Category add

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main', ['title' => 'Cetergory List'])
+
+@section('content')
+    <h1>Category List</h1>
+
+@endsection
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('profile.index') }}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
@@ -19,8 +19,9 @@
                         Dropdown
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                        <li><a class="dropdown-item" href="{{ route('register') }}">Reginster</a></li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
