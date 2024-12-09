5edb3bd6866ce06ab880634eabd744a57805df7a
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-12-09 23:57:41 +0600

    Seeder file added.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,61 @@
+<!-- <//?//php -->
+
+// namespace Database\Seeders;
+
+// use App\Models\User;
+// // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Seeder;
+
+// class DatabaseSeeder extends Seeder
+// {
+//     /**
+//      * Seed the application's database.
+//      */
+//     public function run(): void
+//     {
+//         // User::factory(10)->create();
+
+//         User::factory()->create([
+//             'name' => 'Test User',
+//             'email' => 'test@example.com',
+//         ]);
+//     }
+// }
 <?php
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+           [
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+           ],
+           [
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+           ],
+           [
+            'name' => 'Retailer',
+            'email' => 'retailer@retailer.com',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+           ]
         ]);
     }
 }
